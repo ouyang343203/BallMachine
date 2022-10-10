0261ab59dc604d49b836fd1d4dbe27766568283f
yangwen <ouy15818731124@gmail.com>
2022-10-10 16:39:26 +0800

    sdk更换

--- BallMachine/ViewController.m
+++ BallMachine/ViewController.m
@@ -31,6 +31,6 @@
                           @"versionNumber":@"0.0.1",
                           @"navagation":navagation,
     };
-    [[PluginUpdateManager shareManager]startH5ViewControllerWithNebulaParams:Dic];
+    [[PluginUpdateManager shareManager] startH5ViewControllerWithNebulaParams:Dic withController:self];
 }
 @end
